f2495085277e4471cac2e8a92ad110b91a600122
mohitsharma <mohitsharma900156@gmail.com>
2023-07-26 22:57:25 +0530

    done

--- screen/blogs/AddBlogs.js
+++ screen/blogs/AddBlogs.js
@@ -1,19 +1,49 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { TextInput } from "react-native";
-import { StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, TextInput, Button, Alert, StyleSheet } from "react-native";
 
 const AddBlogs = () => {
-  const [text, setText] = React.useState("");
+  const [inputValue, setInputValue] = useState("");
+  const addDataToServer = async () => {
+    console.warn("test")
+    const url = 'http://localhost:3000/users'; 
+    const dataToSend = [{
+      name: "mohit sharma",
+      age : 20
+    }];
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dataToSend),
+      });
+response =await response.json()
+console.warn(response)
+    } catch (error) {
+      console.error('POST Request Failed');
+      console.error('Error:', error);
+      // Handle errors here
+    }
+  };
   return (
-    <View>
-      <TextInput placeholder="Title ...." style={styles.input} />
-      <TextInput
+    <>
+      <View>
+        <TextInput
+          placeholder="Title ...."
+          style={styles.input}
+          onChangeText={(text) => setInputValue(text)}
+          value={inputValue}
+        />
+        {/* <TextInput
         placeholder="Description ...."
         style={styles.inputDes}
         multiline={true}
-      />
-    </View>
+      /> */}
+        <Button title="Post Data" onPress={addDataToServer} />
+      </View>
+    </>
   );
 };
 
